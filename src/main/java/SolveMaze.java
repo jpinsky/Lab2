9e804246d96a303c8ae53f90632f745fb2540db3
jpinsky <pinsky3@illinois.edu>
2018-09-12 12:10:58 -0500

    Solved SolveMaze

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -20,7 +20,7 @@ public class SolveMaze {
         /*
          * Create a new 10 x 10 maze. Feel free to change these values.
          */
-        Maze maze = new Maze(10, 10);
+        Maze maze = new Maze(5, 5);
 
         /*
          * Pick (0, 0), the bottom left corner, as the starting point.
@@ -33,12 +33,41 @@ public class SolveMaze {
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
          */
+        int numSteps = 0;
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    while (!maze.canMove()) {
+                        maze.turnRight();
+                    }
+                    maze.move();
+                }
+                numSteps++;
+            }
+//            Below is a random walk solution
+//            while (!maze.isFinished()) {
+//                if (maze.canMove()) {
+//                    maze.move();
+//                }
+//                else {
+//                    if (Math.random() > .5) {
+//                        maze.turnLeft();
+//                    }
+//                    else {
+//                        maze.turnRight();
+//                    }
+//                }
+//                numSteps++;
+//            }
+
         }
 
         if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
+            System.out.println("You solved the maze in " + numSteps + " steps!");
         } else {
             System.out.println("Try again!");
         }
